4f1cf9eaba649f38cd0d7f3e5913e353ac391054
Ryouse1 <ryouseikoseki1114@gmail.com>
2025-09-18 08:35:13 +0900

    Update server.js

--- server/scr/server.js
+++ server/scr/server.js
@@ -1,12 +1,19 @@
+// server.js
 const express = require('express');
 const path = require('path');
+
 const app = express();
+
+// Reactなどのビルド成果物を返す場合
+app.use(express.static(path.join(__dirname, 'cilent/build')));
+
+// APIがある場合はここに app.get('/api/xxx', ...) など
+
+// Reactのルートハンドラ
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'cilent/build', 'index.html'));
+});
+
+// ポートは必ず process.env.PORT
 const PORT = process.env.PORT || 3000;
-
-// /entry などアクセスされても何も表示しない
-app.get('/entry', (req, res) => res.send(''));
-
-// 他の存在しない URL も空
-app.get('*', (req, res) => res.send(''));
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
