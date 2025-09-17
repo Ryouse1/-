a5d2f16648c62a920bb3777ef18068f027f365e2
Ryouse1 <ryouseikoseki1114@gmail.com>
2025-09-17 21:28:48 +0900

    Update server.js

--- server/scr/server.js
+++ server/scr/server.js
@@ -3,16 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../../public')));
-
-// ルートアクセスで main index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
+// 通常のエントリーページ
+app.use('/entry', express.static(path.join(__dirname, '../../entry')));
+app.get('/entry', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../entry/index.html'));
 });
 
-// 新しい HTML を /second で配信
-app.get('/second', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/second.html'));
+// 存在しないページにアクセスした場合のダミー
+app.get('*', (req, res) => {
+  res.send('<h1>404 - ページが存在しません</h1><p>保険用のダミーです</p>');
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
