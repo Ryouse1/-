52b01e1507fdbeb5c1ee489412f4067bc229d48f
Ryouse1 <ryouseikoseki1114@gmail.com>
2025-09-17 21:17:50 +0900

    Update server.js

--- server/scr/server.js
+++ server/scr/server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Reactのビルド済み dist 配信
+// ビルド済み React を配信
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// SPA対応
+// SPA ルーティング対応
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
