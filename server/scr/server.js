df18f5c6347885a615e8984a7585ad797933ece5
Ryouse1 <ryouseikoseki1114@gmail.com>
2025-09-17 21:26:37 +0900

    Update server.js

--- server/scr/server.js
+++ server/scr/server.js
@@ -3,15 +3,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// public フォルダを静的配信
 app.use(express.static(path.join(__dirname, '../../public')));
-// server.js から見て ../../public が /-/public に対応
 
-// ルートアクセスは index.html を返す
-app.get('*', (req, res) => {
+// ルートアクセスで main index.html
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// 新しい HTML を /second で配信
+app.get('/second', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/second.html'));
 });
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
